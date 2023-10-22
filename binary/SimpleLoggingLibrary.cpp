52868ae05fae477b2736943ffe1fb10361dee3f5
Damian Zeleny <dzeleny@damiansoftware.com>
2023-10-22 16:38:26 +0200

    Frst commit

---
+++ binary/SimpleLoggingLibrary.cpp
@@ -0,0 +1,12 @@
+﻿// SimpleLoggingLibrary.cpp: Definuje vstupní bod pro aplikaci.
+//
+
+#include "SimpleLoggingLibrary.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
